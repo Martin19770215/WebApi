29cda1d8e4b69e5cbc4109d06d9636a792f2736e
COMPANY-NOTEBOOK <NOTEBOOK@COMPANY.COM>
2026-09-06 21:14:23 +0800

    2026-09-06

--- WebApi/Dals/CustomerDAL.cs
+++ WebApi/Dals/CustomerDAL.cs
@@ -194,15 +194,19 @@ namespace WebApi.Dals
             {
                 if (!string.IsNullOrEmpty(ModuleInfo.SettingURL))
                 {
-                    //string responseString = ws_comm.Get(ModuleInfo.SettingURL, $"owner={ModuleInfo.MainLableName}&mType={ModuleInfo.MTType}&delayFlag=1");
+                    string responseString = ws_comm.Get(ModuleInfo.SettingURL, $"owner={ModuleInfo.MainLableName}&mType={ModuleInfo.MTType}&delayFlag=1");
                     //string responseString = ws_comm.Get(ModuleInfo.SettingURL, $"PluginName=PAMM&MainLableName={ModuleInfo.MainLableName}&MTType=MT5");
-                    //JavaScriptSerializer Serializer = new JavaScriptSerializer();
-                    //List<PluginRemoteInfo> pluginList = Serializer.Deserialize<List<PluginRemoteInfo>>(responseString);
-                    //pluginList.ForEach(info => {
-                    //    lstResult = new CommonDAL().getJsonFromXML(ws_comm.Get(info.fileUrl, ""), ModuleInfo);
-                    //});
+                    JavaScriptSerializer Serializer = new JavaScriptSerializer();
+                    List<PluginRemoteInfo> pluginList = Serializer.Deserialize<List<PluginRemoteInfo>>(responseString);
+                    pluginList.ForEach(info =>
+                    {
+                        lstResult = new CommonDAL().getJsonFromXML(ws_comm.Get(info.fileUrl, ""), ModuleInfo);
+                    });
                     //lstResult = new CommonDAL().getJsonFromXML(ws_comm.Get("http://portal.logicnx.com/Content/UploadFile/PLUGIN/Dragonstone_Forex_Trading_Limited/PAMM_Delay_Config.xml", ""), ModuleInfo);
-                    lstResult = new CommonDAL().getJsonFromXML("http://portal.logicnx.com/Content/UploadFile/PLUGIN/pamm12/PAMM_Config.xml", ModuleInfo);
+                    //lstResult = new CommonDAL().getJsonFromXML("http://portal.logicnx.com/Content/UploadFile/PLUGIN/pamm12/PAMM_Config.xml", ModuleInfo);
+                }
+                else {
+                    lstResult = getCustomeSlaveAccount(ModuleInfo);
                 }
             }
             catch (Exception ex)
@@ -316,5 +320,101 @@ namespace WebApi.Dals
         }
 
         #endregion
+
+        private List<SlaveAccount> getCustomeSlaveAccount(PluginModuleInfo ModuleInfo)
+        {
+            List<SlaveAccount> lstResult = new List<SlaveAccount>();
+            List<SymbolRelations> lstSymbols = new List<SymbolRelations>();
+
+            string sSqlSelectMaster = $"SELECT * FROM PAMM_MasterAcc WHERE AccountName in ({ModuleInfo.AccountName}) AND IsDelete='N';";
+            string sSqlSelectSlave = $"SELECT * FROM PAMM_SlaveAcc WHERE AccountName in ({ModuleInfo.AccountName}) AND IsDelete='N';";
+            string sSqlSelectSymbols = $"SELECT * FROM PAMM_SymbolRelations WHERE AccountName in ({ModuleInfo.AccountName}) AND IsDelete='N';";
+
+            //MasterAccount masteracc = new MasterAccount();
+
+            try
+            {
+                //DataSet ds = ws_mysql.ExecuteDataSetBySQL(sSqlSelectMaster, PublicConst.Database);
+                //foreach (DataRow mDr in ds.Tables[0].Rows)
+                //{
+                //    lstResult.Add(new MasterAccount
+                //    {
+                //        Login = UInt64.Parse(mDr["AccNo"].ToString()),
+                //        //IsDelete = mDr["IsDelete"].ToString() == "Y",
+                //        Comment = mDr["Comment"].ToString(),
+                //        //CreateTime = DateTime.Parse(mDr["CreateTime"].ToString()),
+                //        //sCreateTime = mDr["CreateTime"].ToString(),
+                //        SlaveCount = 0,
+                //        Slaves = new List<SlaveAccount>()
+                //    });
+                //}
+                //if (isIncludeSlave)
+                //{
+                DataSet ds = ws_mysql.ExecuteDataSetBySQL(sSqlSelectSymbols, PublicConst.Database);
+                foreach (DataRow mDr in ds.Tables[0].Rows)
+                {
+                    lstSymbols.Add(new SymbolRelations
+                    {
+                        MasterAcc = UInt64.Parse(mDr["MasterAcc"].ToString()),
+                        SlaveAcc = UInt64.Parse(mDr["SlaveAcc"].ToString()),
+                        MasterSymbol = mDr["MasterSymbol"].ToString(),
+                        SlaveSymbol = mDr["SlaveSymbol"].ToString(),
+                    });
+                }
+
+                ds = ws_mysql.ExecuteDataSetBySQL(sSqlSelectSlave, PublicConst.Database);
+                foreach (DataRow mDr in ds.Tables[0].Rows)
+                {
+                    ModeType SlaveMode = (ModeType)Enum.Parse(typeof(ModeType), mDr["Proportion_Type"].ToString());
+                    List<SymbolRelations> lstSymbolTmp = lstSymbols.Where(acc => acc.MasterAcc == UInt64.Parse(mDr["MasterAcc"].ToString()) && acc.SlaveAcc == UInt64.Parse(mDr["SlaveAcc"].ToString())).ToList<SymbolRelations>();
+                    List<SymbolRelationsFinal> lstSymbolFinal = new List<SymbolRelationsFinal>();
+                    if (lstSymbolTmp.Count <= 0) { lstSymbolFinal.Add(new SymbolRelationsFinal() { Key = mDr["MasterAcc"].ToString() + ",*", MasterSymbol = "*", SlaveSymbol = "*" }); }
+                    else
+                    {
+                        lstSymbolTmp.ForEach(sym => {
+                            lstSymbolFinal.Add(new SymbolRelationsFinal()
+                            {
+                                Key = sym.MasterAcc + "," + sym.MasterSymbol,
+                                MasterSymbol = sym.MasterSymbol,
+                                SlaveSymbol = sym.SlaveSymbol
+                            });
+                        });
+                    }
+                    lstResult.Add(new SlaveAccount
+                    {
+                        SlaveLogin = UInt64.Parse(mDr["SlaveAcc"].ToString()),
+                        Reverse = mDr["Reverse"].ToString() == "Y",
+                        Delay = int.Parse(mDr["Delay"].ToString()),
+                        //ProportionType = mDr["ProportionType"].ToString(),
+                        Mode = (int)SlaveMode,
+                        Rate = UInt64.Parse(mDr["Proportion"].ToString()),
+                        Pedding = mDr["IsPedding"].ToString() == "Y",
+                        SL = mDr["IsSL"].ToString() == "Y",
+                        TP = mDr["IsTP"].ToString() == "Y",
+                        //IsFollowClosedOrder = mDr["IsFollowClosedOrder"].ToString() == "Y",
+                        //Tradable = mDr["IsTradable"].ToString() == "Y",
+                        MasterLogin = UInt64.Parse(mDr["MasterAcc"].ToString()),
+                        Symbols = lstSymbolFinal
+                    });
+                }
+                //}
+            }
+            catch (Exception ex)
+            {
+                new CommonDAL().UploadErrMsg(ModuleInfo, new ErrMsg { ErrorMsg = ex.Message, RouteName = "MTWebApi/COPYTRADER_GetMasterList" });
+                lstResult.Clear();
+            }
+            //if (isIncludeSlave)
+            //{
+            //    lstResult.ForEach(master =>
+            //    {
+            //        //master.Slaves = new CommonDAL().DeepCopy<List<SlaveAccount>>(lstSlave.Where(slave => slave.MasterLogin == master.Login).ToList());
+            //        master.Slaves = lstSlave.Where(slave => slave.MasterLogin == master.Login).ToList();
+            //        master.SlaveCount = master.Slaves.Count;
+            //    });
+            //}
+
+            return lstResult;
+        }
     }
 }
